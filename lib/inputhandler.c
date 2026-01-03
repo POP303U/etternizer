7c395ddbcaa47e09c9ef8a922217b7f9f393e4e8
pops <nicoanton.gering@gmail.com>
2026-01-03 23:50:48 +0100

    Fix wrong error handling

--- lib/inputhandler.c
+++ lib/inputhandler.c
@@ -11,6 +11,9 @@ InputHandler *ValidateFiles(const char *path) {
         exit(1);
     }
 
+    // hardcoded png since quaver doesn't use those
+    inputHandler->cdtitle = "assets/cdtitle.png";
+
     char *qua = FindQuaFile(path);
 
     if (!qua) {
@@ -23,29 +26,43 @@ InputHandler *ValidateFiles(const char *path) {
     snprintf(filepath, sizeof(filepath), "%s%s%s", path, SEPERATOR, qua);
 
     char *data = LoadFile(filepath);
+    inputHandler->chart = strdup(filepath);
 
-    inputHandler->chart      = strdup(filepath);
-    inputHandler->audio      = ExtractValue(data, "AudioFile");
-    inputHandler->BG         = ExtractValue(data, "BackgroundFile");
-    inputHandler->BN         = ExtractValue(data, "BannerFile");
+    snprintf(filepath, sizeof(filepath), "%s%s%s", path, SEPERATOR, ExtractValue(data, "AudioFile"));
+    if (ExtractValue(data, "AudioFile") == NULL) {
+        inputHandler->audio = NULL;
+    } else {
+        inputHandler->audio = strdup(filepath);
+    }
+
+    snprintf(filepath, sizeof(filepath), "%s%s%s", path, SEPERATOR, ExtractValue(data, "BackgroundFile"));
+    if (ExtractValue(data, "BackgroundFile") == NULL) {
+        inputHandler->BG = NULL;
+    } else {
+        inputHandler->BG = strdup(filepath);
+    }
+
+    snprintf(filepath, sizeof(filepath), "%s%s%s", path, SEPERATOR, ExtractValue(data, "BannerFile"));
+    if (ExtractValue(data, "BannerFile") == NULL) {
+        inputHandler->BN = NULL;
+    } else {
+        inputHandler->BN = strdup(filepath);
+    }
 
     // not smart using multiple snprintf calls
-    snprintf(filepath, sizeof(filepath), "%s//%s", path, inputHandler->audio);
-    if (!FileExists(filepath) && inputHandler->audio != NULL) {
+    if (!FileExists(inputHandler->audio) && inputHandler->audio != NULL) {
         inputHandler->valid = 0;
         inputHandler->reason = "No valid audio file found!";
         return inputHandler;
     }
 
-    snprintf(filepath, sizeof(filepath), "%s//%s", path, inputHandler->BG);
-    if (!FileExists(filepath) && inputHandler->BG != NULL) {
+    if (!FileExists(inputHandler->BG) && inputHandler->BG != NULL) {
         inputHandler->valid = 0;
         inputHandler->reason = "No valid background file found!";
         return inputHandler;
     }
 
-    snprintf(filepath, sizeof(filepath), "%s//%s", path, inputHandler->BN);
-    if (!FileExists(filepath) && inputHandler->BN != NULL) {
+    if (!FileExists(inputHandler->BN) && inputHandler->BN != NULL) {
         inputHandler->valid = 0;
         inputHandler->reason = "No valid banner file found!";
         return inputHandler;
